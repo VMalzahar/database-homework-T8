e7764243a37f03ef60ca723dadb8df8d8128598c
Malzahar <1064623716@qq.com>
2023-12-21 14:47:54 +0800

    add login_check.py

---
+++ main/login_check.py
@@ -0,0 +1,8 @@
+from typing import Dict
+
+class Login_check():
+    def __init__(self) -> None:
+        pass
+    def check(self,username,password) -> bool:
+        if username=="admin" and password=="123456":
+            return 1
