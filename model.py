37000c549ddadc32dfd872ced07d46607c208908
zojkloyn <2687553413@qq.com>
2023-12-21 19:38:06 +0800

    user, grant

--- model.py
+++ model.py
@@ -1,44 +1,47 @@
 import pymysql
-class DB:
+class USER:
     pass
 
 
+# self.lang 为 {id:name} 如 {1:'C++',2:'Rust',3:'Python'}
+# self.status 为 {id:name} 如 {1:'Pending',2:'Wrong Answer',3:'RunningTimeError'}
+class DataBase:
+    pass
 
-# 登录，返回数据库类型，登陆失败则返回返回None
-def login(user:str,password:str)->DB:
-    global langs,status
+# 数据库登录至管理员账号，登陆失败则返回None
+def DB_login(dbuser:str,password:str)->DataBase:
     return None
 
+# 用户登录，登陆失败则返回None
+def USER_login(self,db:DataBase,user:str,password:str)->USER:
+    pass
 
-# 静态变量部分
-# DB.lang 为 {id:name} 如 {1:'C++',2:'Rust',3:'Python'}
-# DB.status 为 {id:name} 如 {1:'Pending',2:'Wrong Answer',3:'RunningTimeError'}
-# DB.fetch_code_limit 为是否限制读取其他用户的代码
-
-class DB:
-    fetch_code_limit=True
-
+class USER:
     # 提交，返回提交成功与否
-    def submit(lang_id:int,code:str)->bool:
+    def submit(self,lang_id:int,code:str)->bool:
         pass
 
     # 返回的记录类型
     class record:
+        submit_id=0
+        time_slot=0
+        # 题目编号
+        problem_id=0
         # 提交用户
         user_id=0
         user_name=''
-        # 提交结果
-        status_id=0
-        status_name=''
         # 提交语言
         language_id=0
         language_name=''
-        # 题目编号
-        problem_id=0
+        # 提交结果
+        status_id=0
+        status_name=''
+        code_len=0
+        time=0
 
     # 按要求选择提交记录，返回成功与否
     # 传入参数为空，则表示不做限制
-    def select_record(
+    def select_record(self,
             submit_ids:list[int] = [],
             user_ids:list[int] = [],
             status_ids:list[int] = [],
@@ -47,18 +50,19 @@ class DB:
             )->bool:
         pass
     # 获取一条提交记录，若取完则返回None
-    def fetchone()->record:
+    def fetchone(self)->record:
         pass
     # 获取多条提交记录
-    def fetchall()->list[record]:
+    def fetchall(self)->list[record]:
         pass
-    def fetchmany(x:int = 1)->list[record]:
+    def fetchmany(self,x:int = 1)->list[record]:
         pass
 
     # 获取提交记录的代码
-    def fetch_code(submit_id:int)->str:
+    def fetch_code(self,submit_id:int)->str:
         pass
 
     # 获取题面
-    def fetch_problem(problem_id:int)->bytes:
+    def fetch_problem(self,problem_id:int)->bytes:
         pass
+
