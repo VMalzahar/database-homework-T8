0b64a4c32902621ea8db196c0a502da21c3f5e69
zojkloyn <2687553413@qq.com>
2023-12-25 23:12:25 +0800

    improve model.py

--- model.py
+++ model.py
@@ -2,24 +2,27 @@ import MySQLdb
 import MySQLdb.cursors as cors
 
 # 进行数据库连接内容设置，返回测试连接成功与否
-def DB_set(connection:str = "localhost",dbuser:str,password:str,database:str)->bool:
+def DB_set(dbuser:str,password:str,database:str,connection:str = "localhost")->bool:
     pass
 # 用户登录，登陆失败则返回None
-def USER_login(self,user:str,password:str)->DB:
+# 也推荐使用DB(user,password)建立连接，登陆失败会报异常
+def USER_login(user:str,password:str)->DB:
     pass
 
 # self.lang 为 {id:name} 如 {1:'C++',2:'Rust',3:'Python'}
 # self.status 为 {id:name} 如 {1:'Pending',2:'Wrong Answer',3:'RunningTimeError'}
+# self.user_name 为用户昵称
 class DB:
     # 修改，只有管理员账户能够成功
-    # alter_column 为修改项目，参考class record中非`*_name`项
-    # alter_record 为具体修改，支持多个记录同时修改，格式为 [(submit_id,alter), (submit_id,alter), ...]
-    def alter(self,
-              alter_column:str,
-              alter_record:list[(int,)])->bool:
+    # update_column 为修改项目，参考class record中非`*_name`项
+    # update_record 为具体修改，支持多个记录同时修改，格式为 [(update_thing,submit_id), (update_thing,submit_id), ...]
+    # 不支持改 code
+    def update(self,
+              update_column:str,
+              update_record:list[(any,int)])->bool:
         pass
     # 提交，返回提交成功与否
-    def submit(self,lang_id:int,code:str)->bool:
+    def submit(self,problem_id:int,lang_id:int,code:str)->bool:
         pass
 
     # 返回的记录类型
@@ -39,6 +42,8 @@ class DB:
         status_name=''
         code_len=0
         time=0
+        # 提交代码，若权限不足则为 None
+        code=None
 
     # 按要求选择提交记录，返回成功与否
     # 传入参数为空，则表示不做限制
@@ -50,13 +55,11 @@ class DB:
             language_ids:list[int] = []
             )->bool:
         pass
-    # 获取一条提交记录，若取完则返回None
-    def fetchone(self)->record:
-        pass
-    # 获取多条提交记录
+    # 获取所有提交记录
     def fetchall(self)->list[record]:
         pass
-    def fetchmany(self,x:int = 1)->list[record]:
+    # 获取从 x 开始 num 条提交记录
+    def fetchmany(self,x:int = 1,num:int = 100)->list[record]:
         pass
 
     # 获取提交记录的代码
